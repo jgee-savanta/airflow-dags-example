07a4b2a6d7271264e4cdd4498ea67634829edbd2
jgee-savanta <jack.gee@savanta.com>
2025-06-12 13:26:21 +0100

    use the dag_id tag property

--- dags/beta/test_azure_mssql_connection.py
+++ dags/beta/test_azure_mssql_connection.py
@@ -2,7 +2,7 @@ from airflow import DAG
 from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
 from datetime import datetime
 
-with DAG('beta.test_azure_mssql_connection', start_date=datetime(2024, 1, 1), schedule=None, catchup=False) as dag:
+with DAG(dag_id='beta.test_azure_mssql_connection', start_date=datetime(2024, 1, 1), schedule=None, catchup=False) as dag:
     task = SQLExecuteQueryOperator(
         task_id='run_select',
         sql="SELECT GETDATE();",
