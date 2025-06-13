d6c814c8ec62e99cc01afa4eef159151061caea6
jgee-savanta <jack.gee@savanta.com>
2025-06-13 11:23:25 +0100

    try writing PEM to a tempfile

--- dags/beta/test_snowflake_connection.py
+++ dags/beta/test_snowflake_connection.py
@@ -1,19 +1,25 @@
 import os
+import tempfile
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 import snowflake.connector
 from datetime import datetime
 
 def test_snowflake_conn_fn(**kwargs):
-    # Create connection directly using snowflake.connector
+    private_key_pem = f"-----BEGIN ENCRYPTED PRIVATE KEY-----\n{os.environ.get('SNOWFLAKE_PRIVATE_KEY')}\n-----END ENCRYPTED PRIVATE KEY-----"
+
+    with tempfile.NamedTemporaryFile(delete=False, mode='w') as key_file:
+        key_file_path = key_file.name
+        key_file.write(private_key_pem)
+
     conn = snowflake.connector.connect(
         account="YNDSYIO-SAVANTAUK",
         user="AZURE_CONNECTOR",
         warehouse="WAREHOUSE_XSMALL",
         database="BRANDVUEMETA_TEST",
         role="SYSADMIN",
-        private_key=os.environ.get('SNOWFLAKE_PRIVATE_KEY'),
-        password=os.environ.get('SNOWFLAKE_KEY_ENCRYPTION_PASSWORD')
+        private_key_file=key_file_path,
+        private_key_file_pwd=os.environ.get('SNOWFLAKE_KEY_ENCRYPTION_PASSWORD')
     )
     
     cursor = conn.cursor()
