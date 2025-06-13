a2e479ddeeaa2761815173b9706565af465f80fe
jgee-savanta <jack.gee@savanta.com>
2025-06-13 10:58:50 +0100

    test: move connection config to the DAG

--- dags/beta/test_snowflake_connection.py
+++ dags/beta/test_snowflake_connection.py
@@ -1,10 +1,25 @@
+import os
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.providers.snowflake.hooks.snowflake import SnowflakeHook
 from datetime import datetime
 
 def test_snowflake_conn_fn(**kwargs):
-    hook = SnowflakeHook(snowflake_conn_id='snowflake_meta_test')
+    private_key = f"-----BEGIN ENCRYPTED PRIVATE KEY-----\n{os.environ.get('SNOWFLAKE_PRIVATE_KEY')}\n-----END ENCRYPTED PRIVATE KEY-----"
+    private_key_passphrase = os.environ.get('SNOWFLAKE_KEY_ENCRYPTION_PASSWORD')
+    
+    # Create connection parameters
+    conn_params = {
+        "account": "YNDSYIO-SAVANTAUK",
+        "user": "AZURE_CONNECTOR",
+        "warehouse": "WAREHOUSE_XSMALL",
+        "database": "BRANDVUEMETA_TEST",
+        "role": "SYSADMIN",
+        "private_key": private_key,
+        "private_key_passphrase": private_key_passphrase
+    }
+
+    hook = SnowflakeHook(snowflake_conn_id=None, **conn_params)
     conn = hook.get_conn()
     cursor = conn.cursor()
     cursor.execute("SELECT CURRENT_VERSION();")   # any lightweight query
