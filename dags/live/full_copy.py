32461af53d0cfa0b5658852c0e9cd327ba0256ce
jgee-savanta <jack.gee@savanta.com>
2025-06-16 15:56:19 +0100

    test passed: update other DAGs to use snowflake context manager util function

--- dags/live/full_copy.py
+++ dags/live/full_copy.py
@@ -1,18 +1,14 @@
-import os
-import tempfile
 import pandas as pd
-import snowflake.connector
 from snowflake.connector.pandas_tools import write_pandas
 from airflow import DAG
 from airflow.providers.microsoft.mssql.hooks.mssql import MsSqlHook
-from airflow.providers.snowflake.hooks.snowflake import SnowflakeHook
 from airflow.operators.python import PythonOperator
 from datetime import datetime
+from utils.snowflake_utils import get_snowflake_connection
 
 
 def copy_sqlserver_to_snowflake(**context):
     mssql_conn_id = 'azure_sql_vm'
-    
     source_table = 'dbo.Features'
     target_table = 'BrandVueMeta_Test.vue.Features'.upper()
     
@@ -21,38 +17,23 @@ def copy_sqlserver_to_snowflake(**context):
     df = mssql_hook.get_pandas_df(f"SELECT * FROM {source_table}")
     df.rename(columns=lambda x: x.upper(), inplace=True)  # Ensure column names are uppercase to match Snowflake
     
-    # Step 2: Truncate Snowflake table
-    private_key_pem = f"-----BEGIN ENCRYPTED PRIVATE KEY-----\n{os.environ.get('SNOWFLAKE_PRIVATE_KEY')}\n-----END ENCRYPTED PRIVATE KEY-----"
+    with get_snowflake_connection() as conn:
+        cursor = conn.cursor()
 
-    with tempfile.NamedTemporaryFile(delete=False, mode='w') as key_file:
-        key_file_path = key_file.name
-        key_file.write(private_key_pem)
+        # Step 2: Truncate Snowflake table
+        cursor.execute(f"TRUNCATE TABLE {target_table}")
 
-    conn = snowflake.connector.connect(
-        account="YNDSYIO-SAVANTAUK",
-        user="AZURE_CONNECTOR",
-        warehouse="WAREHOUSE_XSMALL",
-        database="BRANDVUEMETA_TEST",
-        role="SYSADMIN",
-        private_key_file=key_file_path,
-        private_key_file_pwd=os.environ.get('SNOWFLAKE_KEY_ENCRYPTION_PASSWORD')
-    )
+        # Step 3: Load data into Snowflake
+        success = write_pandas(
+            conn=conn,
+            df=df,
+            table_name=target_table.split('.')[-1],
+            schema=target_table.split('.')[-2],
+            database=target_table.split('.')[-3],
+            quote_identifiers=True,
+        )
+        assert success, f"Data load to Snowflake failed for table {target_table}"
 
-    cursor = conn.cursor()
-    truncate_sql = f"TRUNCATE TABLE {target_table}"
-    cursor.execute(truncate_sql)
-
-    # Step 3: Load data into Snowflake
-    success = write_pandas(
-        conn=conn,
-        df=df,
-        table_name=target_table.split('.')[-1],
-        schema=target_table.split('.')[-2],
-        database=target_table.split('.')[-3],
-        quote_identifiers=True,
-        # auto_create_table=True, 
-    )
-    assert success, f"Data load to Snowflake failed for table {target_table}"
 
 with DAG(dag_id='live.fullcopy_sqlserver_to_snowflake', tags=['live'], start_date=datetime(2024, 1, 1), schedule=None, catchup=False) as dag:
     copy_task = PythonOperator(
--- dags/live/test_snowflake_connection.py
+++ dags/live/test_snowflake_connection.py
@@ -1,33 +1,14 @@
-import os
-import tempfile
 from airflow import DAG
 from airflow.operators.python import PythonOperator
-import snowflake.connector
+from utils.snowflake_utils import get_snowflake_connection
 from datetime import datetime
 
 def test_snowflake_conn_fn(**kwargs):
-    private_key_pem = f"-----BEGIN ENCRYPTED PRIVATE KEY-----\n{os.environ.get('SNOWFLAKE_PRIVATE_KEY')}\n-----END ENCRYPTED PRIVATE KEY-----"
-
-    with tempfile.NamedTemporaryFile(delete=False, mode='w') as key_file:
-        key_file_path = key_file.name
-        key_file.write(private_key_pem)
-
-    conn = snowflake.connector.connect(
-        account="YNDSYIO-SAVANTAUK",
-        user="AZURE_CONNECTOR",
-        warehouse="WAREHOUSE_XSMALL",
-        database="BRANDVUEMETA_TEST",
-        role="SYSADMIN",
-        private_key_file=key_file_path,
-        private_key_file_pwd=os.environ.get('SNOWFLAKE_KEY_ENCRYPTION_PASSWORD')
-    )
-    
-    cursor = conn.cursor()
-    cursor.execute("SELECT CURRENT_VERSION();")
-    result = cursor.fetchone()
-    print("Connection OK, Snowflake version:", result[0])
-    cursor.close()
-    conn.close()
+    with get_snowflake_connection() as conn:
+        cursor = conn.cursor()
+        cursor.execute("SELECT CURRENT_VERSION();")
+        result = cursor.fetchone()
+        print("Connection OK, Snowflake version:", result[0])
 
 
 with DAG(dag_id='live.test_snowflake_connection', tags=['live'], start_date=datetime(2024, 1, 1), schedule=None, catchup=False) as dag:
